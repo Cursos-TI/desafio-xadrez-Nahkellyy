4ffd18c7c79011b0f17e0a6a646bbd31cec1d656
Nahkellyy <111392878+Nahkellyy@users.noreply.github.com>
2025-04-27 20:07:09 +0000

    atualização

--- xadrez.c
+++ xadrez.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
     // Nível Novato - Movimentação das Peças
@@ -13,41 +10,23 @@ int main() {
 
      for (int i = 0; i <= 4; i++)// Implementação de Movimentação do Bispo
     {
-        printf("baixo, direita\n");// Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
+        printf("Bispo:cima, direita\n");
     }                   
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
+    // Implementação de Movimentação da Torre.
      while (torre > 0 )
     {
-        printf("direita\n");
+        printf("Torre: direita\n");
         torre--;
     }
     
-
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-
+    //movimento da rainha
     while (rainha > 0 )
     {
-        printf("esquerda\n");
+        printf("Rainha: esquerda\n");
         rainha--;
     }
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
 
     return 0;
 }
